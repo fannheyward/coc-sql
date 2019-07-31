fe1a5e6654232a6e965df08cef56ac6d37254f64
Heyward Fann <fannheyward@gmail.com>
2019-07-31 19:21:46 +0800

    feat(diagnostic): diagnostic works

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { commands, ExtensionContext, languages, workspace } from 'coc.nvim';
-import { Range, Diagnostic, Disposable, DocumentSelector, TextEdit } from 'vscode-languageserver-protocol';
-import SQLFormattingEditProvider, { format, fullDocumentRange } from './SQLFormattingEditProvider';
 import { Parser } from 'node-sql-parser';
-import { Position, DiagnosticSeverity } from 'vscode-languageserver-types';
+import { DiagnosticSeverity, Disposable, DocumentSelector, Position, Range, TextEdit } from 'vscode-languageserver-protocol';
+import SQLFormattingEditProvider, { format, fullDocumentRange } from './SQLFormattingEditProvider';
 
 interface Selectors {
   rangeLanguageSelector: DocumentSelector;
@@ -69,69 +68,33 @@ export async function activate(context: ExtensionContext): Promise<void> {
 
   workspace.onDidOpenTextDocument(
     async e => {
+      const doc = await workspace.document;
+      if (workspace.match(['sql'], doc.textDocument) <= 0) {
+        return;
+      }
+
       try {
         const parser = new Parser();
         parser.parse(e.getText());
       } catch (err) {
         if (err.name !== 'SyntaxError') {
-          console.error(err);
           return;
         }
 
         const start = Position.create(err.location.start.line - 1, err.location.start.column);
         const end = Position.create(err.location.end.line - 1, err.location.end.column);
         const range = Range.create(start.line, start.character, end.line, end.character);
-        Diagnostic.create(range, err.message, DiagnosticSeverity.Error);
-        let diagnostics: Diagnostic[] = [];
-        diagnostics.push({
-          range: range,
-          message: err.message,
-          severity: DiagnosticSeverity.Error,
-          source: 'sql',
-          relatedInformation: []
-        });
 
-        console.log(err);
-        let info = { error: 0, warning: 0, information: 0, hint: 0 };
-        for (let diagnostic of diagnostics) {
-          switch (diagnostic.severity) {
-            case DiagnosticSeverity.Warning:
-              info.warning = info.warning + 1;
-              break;
-            case DiagnosticSeverity.Information:
-              info.information = info.information + 1;
-              break;
-            case DiagnosticSeverity.Hint:
-              info.hint = info.hint + 1;
-              break;
-            default:
-              info.error = info.error + 1;
+        let collection = languages.createDiagnosticCollection('sql');
+        collection.set(e.uri, [
+          {
+            range: range,
+            message: err.message,
+            severity: DiagnosticSeverity.Error,
+            source: 'sql',
+            relatedInformation: []
           }
-        }
-        let doc = await workspace.getDocument(e.uri);
-        workspace.nvim.call('coc#util#set_buf_var', [doc.bufnr, 'coc_diagnostic_info', info], true);
-        workspace.nvim.command('redraws', true);
-        workspace.nvim.call('coc#util#do_autocmd', ['CocDiagnosticChange'], true);
-
-        if (!workspace.nvim.hasFunction('nvim_buf_set_virtual_text')) {
-          return;
-        }
-        let buffer = workspace.nvim.createBuffer(doc.bufnr);
-        let lines: Set<number> = new Set();
-        let srcId = 100;
-        let prefix = '=';
-        buffer.clearNamespace(srcId);
-        for (let diagnostic of diagnostics) {
-          let { line } = diagnostic.range.start;
-          if (lines.has(line)) continue;
-          lines.add(line);
-          let highlight = 'CocError' + 'VirtualText';
-          let msg = diagnostic.message
-            .split(/\n/)
-            .map((l: string) => l.trim())
-            .filter((l: string) => l.length > 0);
-          buffer.setVirtualText(srcId, line, [[prefix + msg, highlight]], {}).logError();
-        }
+        ]);
       }
     },
     null,
