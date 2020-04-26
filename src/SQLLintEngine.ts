a608fd72b3ac4ed3428139a2a2d68ed4a22daf99
Lucas Holder <lucasholderx@gmail.com>
2020-04-26 16:55:10 -0400

    Use pg-query-parser when database is PostgreSQL.

--- src/SQLLintEngine.ts
+++ src/SQLLintEngine.ts
@@ -1,6 +1,10 @@
 import { DiagnosticCollection, languages, workspace } from 'coc.nvim';
 import { Option, Parser } from 'node-sql-parser';
 import { DiagnosticSeverity, Position, Range, TextDocument } from 'vscode-languageserver-protocol';
+import { parse as pgParse } from 'pg-query-parser';
+
+import { getDatabase } from './utils';
+
 
 export class SQLLintEngine {
   private collection: DiagnosticCollection;
@@ -9,35 +13,54 @@ export class SQLLintEngine {
     this.collection = languages.createDiagnosticCollection('sql');
   }
 
-  public async lint(e: TextDocument): Promise<void> {
-    if (workspace.match(['sql'], e) <= 0) {
+  public async lint(textDocument: TextDocument): Promise<void> {
+    if (workspace.match(['sql'], textDocument) <= 0) {
       return;
     }
 
     this.collection.clear();
 
     try {
-      const database = workspace.getConfiguration('sql').get('database') as string;
-      const opt: Option = { database };
-      const parser = new Parser();
-      parser.parse(e.getText(), opt);
-    } catch (err) {
-      if (err.name !== 'SyntaxError') {
-        return;
+      const database = getDatabase();
+      if (database === "postgresql") {
+        // We use pg-query-parser for PostgreSQL
+        const result = pgParse(textDocument.getText());
+        if (result.error) {
+          const { error } = result;
+          const { cursorPosition } = error;
+          error.range = {
+            start: textDocument.positionAt(cursorPosition - 1),
+            end: textDocument.positionAt(cursorPosition),
+          };
+          throw error;
+        }
+      } else {
+        // All other databases use node-sql-parser
+        const parser = new Parser();
+        try {
+          const opt: Option = { database };
+          parser.parse(textDocument.getText(), opt);
+        } catch (err) {
+          if (err.name !== 'SyntaxError') {
+            return;
+          }
+          const start = Position.create(err.location.start.line - 1, err.location.start.column);
+          const end = Position.create(err.location.end.line - 1, err.location.end.column);
+          const range = Range.create(start.line, start.character, end.line, end.character);
+          err.range = range;
+          throw err
+        }
       }
-
-      const start = Position.create(err.location.start.line - 1, err.location.start.column);
-      const end = Position.create(err.location.end.line - 1, err.location.end.column);
-      const range = Range.create(start.line, start.character, end.line, end.character);
-
-      this.collection.set(e.uri, [
-        {
-          range: range,
-          message: err.message,
-          severity: DiagnosticSeverity.Error,
-          source: 'sql',
-          relatedInformation: [],
-        },
+    } catch (err) {
+      const diagnostic = {
+        range: err.range,
+        message: err.message,
+        severity: DiagnosticSeverity.Error,
+        source: 'sql',
+        relatedInformation: [],
+      };
+      this.collection.set(textDocument.uri, [
+        diagnostic,
       ]);
     }
   }
---
+++ src/utils.ts
@@ -0,0 +1,43 @@
+import { workspace } from 'coc.nvim';
+
+import fs from 'fs';
+import { inspect } from 'util';
+
+// util to debug values when developing
+// tail -f /tmp/coc-sql-debug
+export const log = (something: any) => {
+  fs.writeFileSync('/tmp/coc-sql-debug', inspect(something, { depth: null }) + "\n", { flag: 'a' });
+}
+
+const defaultDatabase = "mysql";
+
+const supportedDatabases = [
+  "hive",
+  "mysql",
+  "mariadb",
+  "postgresql",
+  "transactsql",
+];
+
+// Guess database from environment variable DATABASE_URL
+function guessDatabase(): string {
+  if (process.env.DATABASE_URL) {
+    let dbType = process.env.DATABASE_URL.toLowerCase().split(":")[0]
+    if (dbType === "postgres") {
+      dbType = "postgresql"
+    }
+    if (supportedDatabases.includes(dbType)) {
+      return dbType;
+    }
+  }
+  return defaultDatabase;
+}
+
+// gets database from coc config or else, tries to guess value from DATABASE_URL env variable
+export function getDatabase(): string {
+  let database = workspace.getConfiguration('sql').get('database') as string;
+  if (database === "guess") {
+    database = guessDatabase();
+  }
+  return database
+}
