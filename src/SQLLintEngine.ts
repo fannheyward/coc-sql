9e7d63b82b0f54772594b3668ba296da9238076c
Lucas Holder <lucasholderx@gmail.com>
2020-04-27 22:07:24 -0400

    Use pg-query-emscripten for parsing PostgreSQL

--- src/SQLLintEngine.ts
+++ src/SQLLintEngine.ts
@@ -1,6 +1,9 @@
 import { DiagnosticCollection, languages, workspace } from 'coc.nvim';
 import { Option, Parser } from 'node-sql-parser';
 import { DiagnosticSeverity, Position, Range, TextDocument } from 'vscode-languageserver-protocol';
+import PgQuery from 'pg-query-emscripten';
+
+import { getDatabase } from './utils';
 
 export class SQLLintEngine {
   private collection: DiagnosticCollection;
@@ -17,27 +20,46 @@ export class SQLLintEngine {
     this.collection.clear();
 
     try {
-      const database = workspace.getConfiguration('sql').get('database') as string;
-      const opt: Option = { database };
-      const parser = new Parser();
-      parser.parse(textDocument.getText(), opt);
-    } catch (err) {
-      if (err.name !== 'SyntaxError') {
-        return;
+      const database = getDatabase();
+      if (database === "postgresql") {
+        // We use pg-query-parser for PostgreSQL
+        const result = PgQuery.parse(textDocument.getText());
+        if (result.error) {
+          const { error } = result;
+          const cursorPosition = error.cursorpos;
+          error.range = {
+            start: textDocument.positionAt(cursorPosition - 1),
+            end: textDocument.positionAt(cursorPosition),
+          };
+          throw error;
+        }
+      } else {
+        // All other databases use node-sql-parser
+        const parser = new Parser();
+        try {
+          const opt: Option = { database };
+          parser.parse(textDocument.getText(), opt);
+        } catch (err) {
+          if (err.name !== 'SyntaxError') {
+            return;
+          }
+          const start = Position.create(err.location.start.line - 1, err.location.start.column);
+          const end = Position.create(err.location.end.line - 1, err.location.end.column);
+          const range = Range.create(start.line, start.character, end.line, end.character);
+          err.range = range;
+          throw err
+        }
       }
-
-      const start = Position.create(err.location.start.line - 1, err.location.start.column);
-      const end = Position.create(err.location.end.line - 1, err.location.end.column);
-      const range = Range.create(start.line, start.character, end.line, end.character);
-
+    } catch (err) {
+      const diagnostic = {
+        range: err.range,
+        message: err.message,
+        severity: DiagnosticSeverity.Error,
+        source: 'sql',
+        relatedInformation: [],
+      };
       this.collection.set(textDocument.uri, [
-        {
-          range: range,
-          message: err.message,
-          severity: DiagnosticSeverity.Error,
-          source: 'sql',
-          relatedInformation: [],
-        },
+        diagnostic,
       ]);
     }
   }
